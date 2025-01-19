31118769e97f20a7fe82cd06306d62321e985bd8
hiraSyeda <hirasyeda@unimelb.edu.au>
2025-01-19 13:27:40 +1100

    Add arg parser

--- tools/training/test_verified_certified_robust_accuracy.py
+++ tools/training/test_verified_certified_robust_accuracy.py
@@ -1,92 +1,95 @@
 import json
-import sys
-
 import numpy as np
-
+import argparse
 import doitlib
-
-if len(sys.argv) != 5:
-    print(f"Usage: {sys.argv[0]} INTERNAL_LAYER_SIZES certifier_results.json model_weights_csv_dir input_size\n");
-    sys.exit(1)
-
-INTERNAL_LAYER_SIZES=eval(sys.argv[1])
-
-json_results_file=sys.argv[2]
-
-csv_loc=sys.argv[3]+"/"
-
-input_size=int(sys.argv[4])
-
-print(f"Running with internal layer dimensions: {INTERNAL_LAYER_SIZES}")
-print(f"Running with input size: {input_size}")
-
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense
 
-inputs, outputs = doitlib.build_mnist_model(Input, Flatten, Dense, input_size=input_size, internal_layer_sizes=INTERNAL_LAYER_SIZES)
-model = Model(inputs, outputs)
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("internal_layer_sizes", type=str)
+    parser.add_argument("certifier_results", type=str)
+    parser.add_argument("model_weights_csv_dir", type=str)
+    parser.add_argument("input_size", type=int)
+    return parser.parse_args()
 
-print("Building zero-bias gloro model from saved weights...")
+def main():
+    args = parse_arguments()
 
-doitlib.load_and_set_weights(csv_loc, INTERNAL_LAYER_SIZES, model)
+    INTERNAL_LAYER_SIZES = eval(args.internal_layer_sizes)
+    json_results_file = args.certifier_results
+    csv_loc = args.model_weights_csv_dir + "/"
+    input_size = args.input_size
 
-# evaluate hte resulting model
-print("Evaluating the resulting zero-bias gloro model...")
+    print(f"Running with internal layer dimensions: {INTERNAL_LAYER_SIZES}")
+    print(f"Running with input size: {input_size}")
+
+    inputs, outputs = doitlib.build_mnist_model(Input, Flatten, Dense, input_size=input_size, internal_layer_sizes=INTERNAL_LAYER_SIZES)
+    model = Model(inputs, outputs)
+
+    print("Building zero-bias gloro model from saved weights...")
+
+    doitlib.load_and_set_weights(csv_loc, INTERNAL_LAYER_SIZES, model)
+
+    # evaluate hte resulting model
+    print("Evaluating the resulting zero-bias gloro model...")
 
 
-x_test, y_test = doitlib.load_mnist_test_data(input_size=input_size)
+    x_test, y_test = doitlib.load_mnist_test_data(input_size=input_size)
 
-loss, accuracy = model.evaluate(x_test, y_test, verbose=2)
-
-
-print(f"Test Loss (zero bias model): {loss:.4f}")
-print(f"Test Accuracy (zero bias model): {accuracy:.4f}")
+    loss, accuracy = model.evaluate(x_test, y_test, verbose=2)
 
 
-print("Generating output vectors from the test (evaluation) data...")
+    print(f"Test Loss (zero bias model): {loss:.4f}")
+    print(f"Test Accuracy (zero bias model): {accuracy:.4f}")
 
-outputs = model.predict(x_test)
 
-predicted_classes = np.argmax(outputs, axis=1)
-true_classes = np.argmax(y_test, axis=1)
+    print("Generating output vectors from the test (evaluation) data...")
 
-correct_classifications = predicted_classes == true_classes
+    outputs = model.predict(x_test)
 
-robustness=[]
-with open(json_results_file, 'r') as f:
-    robustness = json.load(f)
+    predicted_classes = np.argmax(outputs, axis=1)
+    true_classes = np.argmax(y_test, axis=1)
 
-print("Evaluating Verified Certified Robust Accuracy...\n")
-i=0 # robustness index
-j=0 # correct_classifications index
-count_robust_and_correct=0
-count_robust=0
-count_correct=0
-# the first item in this list is the Lipschitz bounds; others may be debug messages etc.
-assert len(robustness) >= len(correct_classifications)+1
-robustness=robustness[1:]
-assert len(robustness) >= len(correct_classifications)
-n=len(robustness)
-while i<n:
-    r = robustness[i]
-    if "certified" in r:
-        robust = r["certified"]
-        correct = correct_classifications[j]
-        if robust and correct:
-            count_robust_and_correct=count_robust_and_correct+1
-        if robust:
-            count_robust=count_robust+1
-        if correct:
-            count_correct=count_correct+1
-        if i%1000==0:
-            print(f"...done {i} of {n} evaluations...\n");
-        j=j+1
-    i=i+1
+    correct_classifications = predicted_classes == true_classes
 
-assert j==10000
-assert i>=10000
+    robustness=[]
+    with open(json_results_file, 'r') as f:
+        robustness = json.load(f)
 
-print(f"Proportion robust: {float(count_robust)/float(10000)}")
-print(f"Proportion correct: {float(count_correct)/float(10000)}")
-print(f"Proportion robust and correct: {float(count_robust_and_correct)/float(10000)}")
+    print("Evaluating Verified Certified Robust Accuracy...\n")
+    i=0 # robustness index
+    j=0 # correct_classifications index
+    count_robust_and_correct=0
+    count_robust=0
+    count_correct=0
+    # the first item in this list is the Lipschitz bounds; others may be debug messages etc.
+    assert len(robustness) >= len(correct_classifications)+1
+    robustness=robustness[1:]
+    assert len(robustness) >= len(correct_classifications)
+    n=len(robustness)
+    while i<n:
+        r = robustness[i]
+        if "certified" in r:
+            robust = r["certified"]
+            correct = correct_classifications[j]
+            if robust and correct:
+                count_robust_and_correct=count_robust_and_correct+1
+            if robust:
+                count_robust=count_robust+1
+            if correct:
+                count_correct=count_correct+1
+            if i%1000==0:
+                print(f"...done {i} of {n} evaluations...\n");
+            j=j+1
+        i=i+1
 
+    assert j==10000
+    assert i>=10000
+
+    print(f"Proportion robust: {float(count_robust)/float(10000)}")
+    print(f"Proportion correct: {float(count_correct)/float(10000)}")
+    print(f"Proportion robust and correct: {float(count_robust_and_correct)/float(10000)}")
+
+if __name__ == "__main__":
+    main()
